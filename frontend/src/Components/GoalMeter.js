10612a912a2685f56becad385b62e7b7e705285f
lpruiettjr <60082307+lpruiettjr@users.noreply.github.com>
2020-10-29 17:43:31 -0500

    deleted from app

--- frontend/src/Components/GoalMeter.js
+++ frontend/src/Components/GoalMeter.js
@@ -1,36 +1,30 @@
 
-import React, { Component} from 'react';
-import {ProgressBar} from 'react-bootstrap';
-
+import React, { Component, Fragment } from 'react';
+import {ProgressBar, Button} from 'react-bootstrap';
+// import ReactBootstrapStyle from 'react-bootstrap';
+// import ProgressBar from '@bit/react-bootstrap.react-bootstrap.progress-bar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 export default class Goal extends Component {
     constructor(props) {
       super(props);
       this.state = {
-        
+        progress:50
       };
-      // this.api = `http://localhost:8000/api/example`;
+      this.api = `http://localhost:8000/api/example`;
     }
 
 
-
-
-
-
-    
     componentDidMount() {
-      // fetch(this.api)
-      //   .then(res => res.json())
-      //   .then(seaCreatures => {
+      fetch(this.api)
+        .then(res => res.json())
+        .then(seaCreatures => {
          
-      //   });
+        });
     }
 
     
-  
-
-
 
     render() {
 
@@ -40,11 +34,10 @@ export default class Goal extends Component {
         <div id="progressBar-container">
           <div id ='progressBar'>
             
-          <ProgressBar now = {50} />
+          <ProgressBar now = {this.state.progress} variant="success" label={`${this.state.progress}% Success`}/>
 
           </div>
       </div>
       )
     }
   }
-
